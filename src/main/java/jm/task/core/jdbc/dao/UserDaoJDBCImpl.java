510eed9f13605698f7e99e1388b15ed175bb0708
melchakovanton <62509011+melchakovanton@users.noreply.github.com>
2020-06-22 22:42:00 +0500

    second commit

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,106 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection;
+
+    public UserDaoJDBCImpl() {
+
+    }
+
+    public UserDaoJDBCImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void createUsersTable() {
+
+        String sql = "CREATE TABLE if NOT EXISTS users (\n" +
+                "    id INTEGER AUTO_INCREMENT PRIMARY KEY, \n" +
+                "    name VARCHAR(30), \n" +
+                "    lastName VARCHAR(30), \n" +
+                "    age INTEGER\n" +
+                ");";
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE if EXISTS users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+
+        String sql = "INSERT INTO users(name, lastName, age) VALUES (?,?,?)";
+        try (PreparedStatement prstm = connection.prepareStatement(sql)) {
+            prstm.setString(1, name);
+            prstm.setString(2, lastName);
+            prstm.setByte(3, age);
+            prstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void removeUserById(long id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> list = new ArrayList<>();
+
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute("SELECT * FROM users");
+            ResultSet result = stmt.getResultSet();
+
+            while (result.next()) {
+                list.add(new User(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("lastName"),
+                        result.getByte("age")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public void cleanUsersTable() {
+        String sql = "DELETE FROM users ";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
